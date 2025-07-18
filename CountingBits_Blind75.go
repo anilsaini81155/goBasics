393870b06ac1c4dc243964d3533f68f3cfa0ec06
Anil Saini <anilsaini81155@gmail.com>
2025-07-18 18:55:58 +0530

    Create CountingBits_Blind75.go

---
+++ CountingBits_Blind75.go
@@ -0,0 +1,53 @@
+/*
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+
+Example 2:
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+ 
+
+Constraints:
+0 <= n <= 105
+
+Implement above in golang
+*/
+
+
+package main
+
+import (
+	"fmt"
+)
+
+// countBits returns an array where each element i contains
+// the number of 1's in the binary representation of i
+func countBits(n int) []int {
+	ans := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		ans[i] = ans[i>>1] + (i & 1)
+	}
+	return ans
+}
+
+func main() {
+	// Test examples
+	fmt.Println(countBits(2)) // Output: [0 1 1]
+	fmt.Println(countBits(5)) // Output: [0 1 1 2 1 2]
+}
